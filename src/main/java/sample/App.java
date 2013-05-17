509a2d98b907c6844ddf309bbede215d7302b53b
Takashi AOKI <federkasten@me.com>
2013-05-17 19:06:10 +0900

    show rotating/blinking rect

--- src/main/java/sample/App.java
+++ src/main/java/sample/App.java
@@ -5,20 +5,43 @@ import casmi.AppletRunner;
 import casmi.KeyEvent;
 import casmi.MouseButton;
 import casmi.MouseEvent;
+import casmi.graphics.color.Color;
+import casmi.graphics.color.RGBColor;
+import casmi.graphics.element.Rect;
 
 /**
  * casmi main class.
  */
 public class App extends Applet {
 
+    private Rect r;
+    private Color c = new RGBColor("#0572B9");
+    
     @Override
     public void setup() {
-        // Implement here.
+        setSize(1024, 768);
+        setFPS(30);
+        
+        // create rect element, set size, position and fill color, 
+        r = new Rect(320, 240);
+        r.setPosition(512, 384);
+        r.setFillColor(c);
+        
+        // add rect to rendering object tree
+        addObject(r);
     }
 
     @Override
     public void update() {
-        // Implement here.
+        long value = System.currentTimeMillis();
+        
+        // rotate rect
+        double rot = value % (360 * 100);
+        r.setRotation(rot / 100.0, 1.0, 2.0, 1.0);
+        
+        // blink rect
+        c.setAlpha((Math.sin(value / 400.0) + 1.0) / 2.0);
+        r.setFillColor(c);
     }
 
     // Comment out if you want to exec terminating processes.
@@ -35,7 +58,11 @@ public class App extends Applet {
 
     @Override
     public void keyEvent(KeyEvent e) {
-        // Implement here.
+        if (e == KeyEvent.PRESSED) {
+            if (getKeyCode() == 27) {  // if pressed ESC 
+                System.exit(0);
+            }
+        }
     }
 
     public static void main(String[] args) {
